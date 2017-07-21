bdc108acfea105a0079142c7bd6c28ba01f1bbb7
root <root@localhost.localdomain>
2017-07-20 23:27:25 -0400

    Initing

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
 var express = require('express');
 var app = express();
 var mysql = require('mysql');
-
-var connection = mysql.createConnection({
-	host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'videos'
-});
-
+var port = process.env.PORT || 8080 ;
 app.use('/static',express.static(__dirname+'/data'));
-
-connection.connect(function(){
-	 console.log("successfully connected");
-});
-
 
 app.get("/",function(req,res){
 	res.render("home.ejs");
@@ -22,9 +10,8 @@ app.get("/",function(req,res){
 app.get("/books",function(req,res){
    res.render("books.ejs");
 });
-app.listen("4321",function(){
+app.listen( port,function(){
 	console.log("successfully connected");
-	
 });
 app.get("*",function(req,res){
 	res.send("page not found");
