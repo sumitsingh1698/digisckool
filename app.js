dd57ef220b3f43a1ab1cfb0f0c878296ace679d7
root <root@localhost.localdomain>
2017-07-20 23:34:57 -0400

    push your message here

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 var express = require('express');
 var app = express();
-var mysql = require('mysql');
-var port = process.env.PORT || 8080 ;
+var port = process.env.PORT || 8080 
 app.use('/static',express.static(__dirname+'/data'));
 
 app.get("/",function(req,res){
-	res.render("home.ejs");
+	res.send("home");
 });
 app.get("/books",function(req,res){
    res.render("books.ejs");
 });
-app.listen( port,function(){
+app.listen(port,function(){
 	console.log("successfully connected");
 });
-app.get("*",function(req,res){
-	res.send("page not found");
-});
+
